806c8276af8374270acc58eda393695b8c43c39e
9601dani <erickmorales201930699@cunoc.edu.gt>
2025-02-07 13:09:30 -0600

    fix: adding solution codechallenge one, with maybe TDD, this commit it is out of time, it should not be taken into account

---
+++ SolucionCodeChallenge/SolutionCodeChall1/src/test/java/com/danimo/AppTest.java
@@ -0,0 +1,27 @@
+package com.danimo;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void testIsAnagram(){
+        boolean result = App.isAnagram("pesada", "espada");
+        assertTrue(result);
+    }
+
+    @Test
+    void test(){
+        boolean result = App.isAnagram("hola", "colaa");
+        assertFalse(result);
+    }
+
+
+
+
+}
+
