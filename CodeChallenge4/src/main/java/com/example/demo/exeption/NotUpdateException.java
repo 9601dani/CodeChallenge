1734d8c8cc6372fc56b67d119d41788cc8713c28
9601dani <erickmorales201930699@cunoc.edu.gt>
2025-03-13 17:03:29 -0600

    fix: someone fix

--- CodeChallenge4/src/main/java/com/example/demo/exeption/NotUpdateException.java
+++ CodeChallenge4/src/main/java/com/example/demo/exeption/NotUpdateException.java
@@ -1,5 +1,9 @@
 package com.example.demo.exeption;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotUpdateException extends RuntimeException {
     
     public NotUpdateException(String message){
